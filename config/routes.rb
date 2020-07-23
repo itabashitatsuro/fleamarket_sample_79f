72f54d78b53e16626052eb913460231fe904ea8e
sat0chika <sa10chika@gmail.com>
2020-07-23 19:39:40 +0900

    registoration/login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   root "posts#index"
   resources :posts, only: [:index]
+  resources :users do
+    collection do
+      get 'login'
+      get 'new'
+    end
+  end
 end
