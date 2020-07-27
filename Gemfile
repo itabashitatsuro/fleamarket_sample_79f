b455275ea9a7f7d9c4ea7739461c7c5bd77b8309
sat0chika <sa10chika@gmail.com>
2020-07-27 17:59:14 +0900

    fix

--- Gemfile
+++ Gemfile
@@ -55,13 +55,13 @@ group :test do
   gem 'selenium-webdriver'
   # Easy installation and use of web drivers to run system tests with browsers
   gem 'webdrivers'
+
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem "haml-rails", ">= 1.0", '<= 2.0.1'
 gem "jquery-rails"
-gem "font-awesome-rails"
 gem "font-awesome-sass"
 gem 'carrierwave'
 # PAY.JPのgem
--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,11 @@ Rails.application.routes.draw do
   resources :posts, only: [:index]
   resources :addresses, only: [:new, :create, :edit, :update, :destory]
   resources :credit_cards, only: [:new, :create, :destroy, :show]
-  resources :users, only: [:new, :create, :edit, :update, :show]
-    resources :users do
-      collection do
-        get 'login'
-      end
+  resources :users do
+    collection do
+      get 'login'
     end
+  end
   
   resources :items, only: [:index, :new] do
     collection do
@@ -17,8 +16,9 @@ Rails.application.routes.draw do
   end
   resources :items do
     collection do
+      get 'login'
+      get 'new'
       get 'purchase'
     end
   end
 end
-
