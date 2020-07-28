c4a90eac3365425a5b14624877d850c215feff9e
1110025y <spw57h9@gmail.com>
2020-07-29 06:07:50 +0900

    update3

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,17 @@ class Item < ApplicationRecord
   has_many :images
   accepts_nested_attributes_for :images, allow_destroy: true
   has_many :images, dependent: :destroy
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
+
+
+
+  validates :name, length: {maximum: 10}, presence: true
+  validates :price, presence: true
+  validates :introduction, presence: true
+  validates :brand, presence: true
+  validates :prefecture, presence: true
+  validates :shopping_date, presence: true
+
 end
