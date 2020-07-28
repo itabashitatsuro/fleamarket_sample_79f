8c620ad09f416c1051869ce77accd9ffb1aed85b
keitobass <keitobass@icloud.com>
2020-07-28 11:05:33 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,29 @@
 Rails.application.routes.draw do
+<<<<<<< Updated upstream
   # デプロイ作業に必要なルーティング
   root "posts#index"
   resources :posts, only: [:index]
   resources :items, only: [:index, :new]
 end
   resources :users do
+=======
+  root "items#index"
+  resources :posts, only: [:index]
+  resources :addresses, only: [:new, :create, :edit, :update, :destory]
+  resources :credit_cards, only: [:new, :create, :destroy, :show]
+  resources :users do
+    collection do
+      get 'login'
+    end
+  end
+  
+  resources :items, only: [:index, :new] do
+    collection do
+      get 'test'
+    end
+  end
+  resources :items do
+>>>>>>> Stashed changes
     collection do
       get 'login'
       get 'new'
