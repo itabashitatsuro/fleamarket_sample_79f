320e5baf0efb38347297bee542fc9126d6c109fc
1110025y <spw57h9@gmail.com>
2020-07-31 13:21:43 +0900

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,6 +9,17 @@ class Item < ApplicationRecord
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :delivery_fee
 
+
   belongs_to :category
+
+
+  validates :name, presence: true, length: {maximum: 40}
+  validates :price, presence: true
+  validates :introduction, presence: true, length: {maximum: 1000}
+  validates :prefecture, presence: true
+  validates :shopping_date, presence: true
+  validates :delivery_fee, presence: true
+  validates :status, presence: true
+  validates :image, presence: true, length: {manimum: 1, maximum: 10}
   
 end
