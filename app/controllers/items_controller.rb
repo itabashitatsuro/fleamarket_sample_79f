9761e2e3fe301b8d949aa07132c9ff9572725366
1110025y <spw57h9@gmail.com>
2020-07-31 14:43:27 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   
   def index
-    @products = Product.includes(:images).order('created_at DESC')
+    @items = Item.includes(:images).order('created_at DESC')
   end
 
   def new
