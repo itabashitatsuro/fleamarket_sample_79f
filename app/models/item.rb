ee522d0135cad2e8fd677485b67e8ac456c47dd3
1110025y <spw57h9@gmail.com>
2020-07-31 12:55:39 +0900

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,5 @@
 class Item < ApplicationRecord
   # belongs_to: user
-  belongs_to: category
   # has_many: item_images,dependent：：destroy
   # has_many: comments、dependent：：destroy
   # has_many: favorites
@@ -8,5 +7,6 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :shopping_date
 
+  belongs_to :category
   
 end
