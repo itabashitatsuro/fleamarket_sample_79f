04509950799b3cedbd6dc50d4e02fc960b050902
1110025y <spw57h9@gmail.com>
2020-07-31 12:06:04 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_28_083741) do
+ActiveRecord::Schema.define(version: 2020_07_31_030321) do
 
   create_table "credit_cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "user_id", null: false
@@ -33,6 +33,12 @@ ActiveRecord::Schema.define(version: 2020_07_28_083741) do
     t.integer "delivery_fee", null: false
     t.string "area", null: false
     t.integer "buyer_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "shipping_dates", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
