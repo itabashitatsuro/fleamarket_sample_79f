3db18920271f4ee8e49264d660e89064fcca3e6f
1110025y <spw57h9@gmail.com>
2020-07-31 15:29:25 +0900

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,9 +9,11 @@ class Item < ApplicationRecord
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :delivery_fee
 
-
-  belongs_to :category
   has_many :images
+  accepts_nested_attributes_for :images, allow_destroy: true
+  has_many :images, dependent: :destroy
+  
+  belongs_to :category
 
 
   # validates :name, presence: true, length: {maximum: 40}
