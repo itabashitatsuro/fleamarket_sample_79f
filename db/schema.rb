0d8e3a390c10d16f2b97994779a496d0090acaa6
1110025y <spw57h9@gmail.com>
2020-08-04 11:15:44 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_03_084111) do
+ActiveRecord::Schema.define(version: 2020_08_03_215657) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "postal_code", null: false
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 2020_08_03_084111) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "category_id"
+    t.string "brand"
     t.index ["category_id"], name: "index_items_on_category_id"
   end
 
