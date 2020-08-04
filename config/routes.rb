cfe192e0cdac5caa7696089a69ba5a11cae8136d
keitobass <keitobass@icloud.com>
2020-08-04 11:12:43 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,16 +4,10 @@ Rails.application.routes.draw do
 
 
   root "items#index"
-  resources :posts, only: [:index]
   resources :addresses, only: [:new, :create, :edit, :update, :destory]
   resources :credit_cards, only: [:new, :create, :destroy, :show]
-  resources :users
-
-  resources :items, only: [:index, :new] do
-    collection do
-  root "items#index"
   resources :posts, only: [:index]
-  resources :addresses, only: [:new, :create, :edit, :update, :destory]
+  
   resources :users do
     collection do
       get 'login'
@@ -22,26 +16,14 @@ Rails.application.routes.draw do
   
   resources :items do
     collection do
-      get 'category/get_category_children', to: 'items#get_category_children', defaults: { format: 'json' }
-      get 'category/get_category_grandchildren', to: 'items#get_category_grandchildren', defaults: { format: 'json' }
-    end
-  end
-
-  resources :items, only: [:index, :new] do
-    collection do
-      get 'test'
-    end
-  end
-  resources :items do
-    collection do
       get 'test'
       get 'login'
       get 'new'
       get 'purchase'
       get 'pay'
+      get 'category/get_category_children', to: 'items#get_category_children', defaults: { format: 'json' }
+      get 'category/get_category_grandchildren',
     end
   end
-end
 
-  resources :credit_cards, only: [:new, :create, :destroy, :show]
 end
