9111648ef78c095cff871e146f1b078ff6ff78ae
itabashitatsuro <dokidokigorira82@gmail.com>
2020-08-05 20:49:12 +0900

    user-sign-up_fix

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,7 +9,7 @@ class Item < ApplicationRecord
   has_many :images
   accepts_nested_attributes_for :images, allow_destroy: true
   has_many :images, dependent: :destroy
-  
+  belongs_to :user
   belongs_to :category
   # belongs_to :seller, class_name: "User", foreign_key: 'seller_id'
   # belongs_to :buyer, class_name: "User", foreign_key: 'buyer_id'
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ApplicationRecord
   # validates :birthday
   end
 
-  # has_many: items, dependent: :destroy
+  has_many :items, dependent: :destroy
   # has_many: comments、dependent：：destroy
   # has_many: favorites、dependent：：destroy
   # has_one：profile、dependent：：destroy
