5f8658773bf3e2925490f5e104b445f814dd8566
1110025y <spw57h9@gmail.com>
2020-08-07 16:20:23 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_04_090254) do
+ActiveRecord::Schema.define(version: 2020_08_07_070646) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "postal_code", null: false
@@ -54,7 +54,6 @@ ActiveRecord::Schema.define(version: 2020_08_04_090254) do
   end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "user_id"
     t.string "name", null: false
     t.integer "price", null: false
     t.text "introduction"
@@ -66,7 +65,9 @@ ActiveRecord::Schema.define(version: 2020_08_04_090254) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "category_id"
     t.string "brand"
+    t.bigint "user_id"
     t.index ["category_id"], name: "index_items_on_category_id"
+    t.index ["user_id"], name: "index_items_on_user_id"
   end
 
   create_table "prefectures", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -107,4 +108,5 @@ ActiveRecord::Schema.define(version: 2020_08_04_090254) do
 
   add_foreign_key "images", "items"
   add_foreign_key "items", "categories"
+  add_foreign_key "items", "users"
 end
