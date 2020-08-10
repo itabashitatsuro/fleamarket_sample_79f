8f30720eea05939c71f6150286df9c615505b9f9
1110025y <spw57h9@gmail.com>
2020-08-10 09:01:52 +0900

    Update multiple_posts.js

--- app/javascript/multiple_posts.js
+++ app/javascript/multiple_posts.js
@@ -1,36 +1,36 @@
+$(document).on('turbolinks:load', function(){
+  $(function(){
+      //DataTransferオブジェクトで、データを格納する箱を作る
+      var dataBox = new DataTransfer();
+      //querySelectorでfile_fieldを取得し変数に入れている
+      var file_field = document.querySelector('input[type=file]')
+      
+      //file(#image-box)が変化した時に発火するイベント
+      $('#image-box').on("change",`input[type="file"]`,function(){
+      //選択したファイル情報を取得し変数に格納 - 最後の[0]は最初のファイルという意味
+      var files = $('input[type=file]').prop('files')[0];
+      
+      //$.each()メソッドで、配列やハッシュに対して繰り返し処理を行う
+      $.each(this.files, function(i,file){
+      
+        //FileReaderのreadAsDataURLで指定したFileオブジェクトを読み込む
+        var fileReader = new FileReader();
+      
+        //DataTransferオブジェクトに対して、fileを追加
+        dataBox.items.add(file)
+        //dataTransferオブジェクトに入ったfile一覧をfile_fieldの中に代入
+        //file_field.files =  dataBox.files
+        // file_fieldのnameに動的なindexをつける為の配列
+        let num = [1,2,3,4,5,6,7,8];
+        let img = [0,1,2,3,4,5,6,7];
+        // 既に使われているindexを除外
+        lastIndex = $('.input-field__contents:last').data('index');
+        anotherIndex = $('.input-field__contents:last').data('index');
+        //splice()メソッドを使い配列から要素を削除・追加して組み替える
+        num.splice(0, lastIndex);
+        img.splice(0, anotherIndex);
 
-$(function(){
-    //DataTransferオブジェクトで、データを格納する箱を作る
-    var dataBox = new DataTransfer();
-    //querySelectorでfile_fieldを取得し変数に入れている
-    var file_field = document.querySelector('input[type=file]')
-    
-    //file(#image-box)が変化した時に発火するイベント
-    $('#image-box').on("change",`input[type="file"]`,function(){
-    //選択したファイル情報を取得し変数に格納 - 最後の[0]は最初のファイルという意味
-    var files = $('input[type=file]').prop('files')[0];
-    
-    //$.each()メソッドで、配列やハッシュに対して繰り返し処理を行う
-    $.each(this.files, function(i,file){
-    
-      //FileReaderのreadAsDataURLで指定したFileオブジェクトを読み込む
-      var fileReader = new FileReader();
-    
-      //DataTransferオブジェクトに対して、fileを追加
-      dataBox.items.add(file)
-      //dataTransferオブジェクトに入ったfile一覧をfile_fieldの中に代入
-      //file_field.files =  dataBox.files
-    
-
-      // file_fieldのnameに動的なindexをつける為の配列
-      let num = [1,2,3,4,5,6,7,8];
-      let img = [0,1,2,3,4,5,6,7];
-      lastIndex = $('.input-field__contents:last').data('index');
-      anotherIndex = $('.input-field__contents:last').data('index');
-      //splice()メソッドを使い配列から要素を削除・追加して組み替える
-      num.splice(0, lastIndex);
-      img.splice(0, anotherIndex);
-      // 画像用のinputにそれぞれ異なる番号付与する記述
+        // 画像用のinputにそれぞれ異なる番号付与する記述
         // function hello(val) {
         //     val
         // }
@@ -49,81 +49,90 @@ $(function(){
                         </div>`;
           return html;
         }
-    
-      $('#image-box-2').append(buildFileField(num[0]));
+      
+
+        $('#image-box-2').append(buildFileField(num[0]));
 
 
-      //fileReader.readAsDataURL(file)で画像の読み込み。
-      fileReader.readAsDataURL(file);
-    
-      //読み込みが完了すると、srcにfileのURLを格納
-      fileReader.onloadend = function() {
-        //resultプロパティで、読み込み成功後に、中身のデータを取得する
-        var src = fileReader.result
+        //fileReader.readAsDataURL(file)で画像の読み込み。
+        fileReader.readAsDataURL(file);
+      
+        //読み込みが完了すると、srcにfileのURLを格納
+        fileReader.onloadend = function() {
+          //resultプロパティで、読み込み成功後に、中身のデータを取得する
+          var src = fileReader.result
 
-        var html = `<div class='item-image' data-image="${file.name}" data-index="${img[0]}">
-                      <div class=' item-image__content'>
-                        <div class='item-image__content--icon'>
-                          <img src=${src} width="140" height="150" class='item-image__content--icon-a' >
+          var html = `<div class='item-image' data-image="${file.name}" data-index="${img[0]}">
+                        <div class=' item-image__content'>
+                          <div class='item-image__content--icon'>
+                            <img src=${src} width="140" height="150" >
+                          </div>
                         </div>
-                      </div>
-                      <div class='item-image__operetion'>
-                        <div class='item-image__operetion--delete' >削除</div>
-                      </div>
-                    </div>`
-        //image_box__container要素の前にhtmlを差し込む
-        $('#image-box__container').before(html);
-      };
-      //image-box__containerのクラスを変更し、CSSでドロップボックスの大きさを変えてやる。
-      //$('#image-box__container').attr('class', `item-num-${num}`)
-      // $('#image-box__container').after(html)
+                        <div class='item-image__operetion'>
+                          <div class='item-image__operetion--delete' >削除</div>
+                        </div>
+                      </div>`
+          //image_box__container要素の前にhtmlを差し込む
+          $('#image-box__container').before(html);
+        };
+        //image-box__containerのクラスを変更し、CSSでドロップボックスの大きさを変えてやる。
+        //$('#image-box__container').attr('class', `item-num-${num}`)
+        // $('#image-box__container').after(html)
+      });
     });
+
+
+
+    ////削除ボタンをクリックすると発火するイベント
+    $(document).on("click", '.item-image__operetion--delete', function(){
+      // var index = $('.item-image').data('index');
+
+      //item-imageのindexを取得して変数に代入
+      var index = $(this).parents('.item-image').data('index');
+      //プレビュー要素を取得して変数に代入
+      var target_image = $(this).parent().parent().data('index');
+      console.log(index)
+
+      const hiddenCheck = $(`input[data-index="${target_image}"].hidden-destroy`);
+      // もしチェックボックスが存在すればチェックを入れる
+      if (hiddenCheck) hiddenCheck.prop('checked', true);
+      
+      //同じカスタムデーター属性の値を持つものを取り除く
+      $(`#item_images_attributes_${index}_item_image`).remove();
+      //プレビュー削除
+      $(`.item-image[data-index=${target_image}]`).remove();
+    })
+
+
+    
+    
+    // //画像を追加した時に、labelのfor属性の値を新しいinputのidに変える記述
+    // $(document).on("change", '#item_images_attributes_0_item_image', function(){
+    // $('label').attr('for', 'item_images_attributes_1_item_image');
+    // })
+    // $(document).on("change", '#item_images_attributes_1_item_image', function(){
+    // $('label').attr('for', 'item_images_attributes_2_item_image');
+    // })
+    // $(document).on("change", '#item_images_attributes_2_item_image', function(){
+    // $('label').attr('for', 'item_images_attributes_3_item_image');
+    // })
+    // $(document).on("change", '#item_images_attributes_3_item_image', function(){
+    // $('label').attr('for', 'item_images_attributes_4_item_image');
+    // })
+    // $(document).on("change", '#item_images_attributes_4_item_image', function(){
+    // $('label').attr('for', 'item_images_attributes_5_item_image');
+    // })
+
+
   });
-
-
-
-  ////削除ボタンをクリックすると発火するイベント
-  $(document).on("click", '.item-image__operetion--delete', function(){
-    // var index = $('.item-image').data('index');
-
-    //item-imageのindexを取得して変数に代入
-    var index = $(this).parents('.item-image').data('index');
-    //プレビュー要素を取得して変数に代入
-    var target_image = $(this).parent().parent().data('index');
-    console.log(index)
-    //同じカスタムデーター属性の値を持つものを取り除く
-    $(`#item_images_attributes_${index}_item_image`).remove();
-    //プレビュー削除
-    $(`.item-image[data-index=${target_image}]`).remove();
+  $('#image-box-icon').on('click', function() {
+    // debugger
+    var lastIndex = $('.input-field__contents:last').data('index');
+    console.log(lastIndex)
+    //ここで、「trigger」を利用して「クリックイベント」を呼び出します
+    // debugger
+    $(`#item_images_attributes_${lastIndex}_item_image`).trigger('click')
+    console.log($('.input-field__contents:last'))
+    
   })
-
-
-
-  //画像を追加した時に、labelのfor属性の値を新しいinputのidに変える記述
-  $(document).on("change", '#item_images_attributes_0_item_image', function(){
-    var result = $('label').attr('for', 'item_images_attributes_1_item_image');
-  })
-  $(document).on("change", '#item_images_attributes_1_item_image', function(){
-    var result = $('label').attr('for', 'item_images_attributes_2_item_image');
-  })
-  $(document).on("change", '#item_images_attributes_2_item_image', function(){
-    var result = $('label').attr('for', 'item_images_attributes_3_item_image');
-  })
-  $(document).on("change", '#item_images_attributes_3_item_image', function(){
-    var result = $('label').attr('for', 'item_images_attributes_4_item_image');
-  })
-  $(document).on("change", '#item_images_attributes_4_item_image', function(){
-    var result = $('label').attr('for', 'item_images_attributes_5_item_image');
-  })
-  $(document).on("change", '#item_images_attributes_5_item_image', function(){
-    var result = $('label').attr('for', 'item_images_attributes_6_item_image');
-  })
-  $(document).on("change", '#item_images_attributes_6_item_image', function(){
-    var result = $('label').attr('for', 'item_images_attributes_7_item_image');
-  })
-  // if ($(`#image-box-1`).length == 5){
-  //   var deletes = $('#image-box__container')
-  //   deletes.css('display', 'none')
-  // }
-
 });
