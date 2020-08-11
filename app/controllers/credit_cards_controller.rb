207bc42bb5642c0300ef34b64d442477b8bdbc82
itabashitatsuro <dokidokigorira82@gmail.com>
2020-08-11 10:24:11 +0900

    commit

--- app/controllers/credit_cards_controller.rb
+++ app/controllers/credit_cards_controller.rb
@@ -2,15 +2,15 @@ class CreditCardsController < ApplicationController
   require 'payjp'
   
   def new
-    @card = CreditCard.where(user_id: current_user.id)
-    if @card.exists?
-      redirect_to credit_card_path(current_user.id)
-    end
+    # @card = CreditCard.where(user_id: current_user.id)
+    # if @card.exists?
+    #   redirect_to credit_card_path(current_user.id)
+    # end
   end
 
   def create
-    # Payjp.api_key = Rails.application.credentials.dig(:payjp, :PAYJP_SECRET_KEY) 
     Payjp.api_key = "sk_test_c806e554d011ef961a9f1ea5"
+    
     if params["payjp_token"].blank?
       redirect_to action: "new", alert: "クレジットカードを登録できませんでした。"
     else
@@ -21,7 +21,7 @@ class CreditCardsController < ApplicationController
     
       @card = CreditCard.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
       if @card.save
-        # redirect_to credit_card_path(@card), notice: "クレジットカードの登録が完了しました"
+        redirect_to credit_card_path(@card), notice: "クレジットカードの登録が完了しました"
       else
         redirect_to action: "create"
       end
