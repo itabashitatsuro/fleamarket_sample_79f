117fb52a372e1f984302a0ca1e89f415cdb611c0
1110025y <spw57h9@gmail.com>
2020-08-12 19:58:03 +0900

    Update image.rb

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,5 +2,6 @@ class Image < ApplicationRecord
   belongs_to :item
   mount_uploader :item_image, ImageUploader
 
-  validates :image, presence: true, length: {manimum: 1, maximum: 5}
+  # validates :image, presence: true, length: {manimum: 1, maximum: 5}
+  validates :image, presence: true
 end
